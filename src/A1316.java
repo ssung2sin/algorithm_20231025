ff4bb357213d1aa671c03c9a759e0b14ebce8aa2
ssung2sin <ssung2sin@naver.com>
2023-10-25 14:06:13 +0900

    1025 백준풀이

---
+++ src/A1316.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class A1316 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int count=0;
+        char saveChar;
+        for(int i=0;i<=a;i++){
+            boolean flag=true;
+            String s = sc.nextLine();
+            for(int j=0;j<s.length();j++) {
+                char c = s.charAt(j);
+                for (int k = j + 1; k < s.length(); k++) {
+                    if (k == j + 1) {
+                        if (c == s.charAt(k)) {
+                            j++;
+                        }
+                    } else {
+                        if (c == s.charAt(k)) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if (!flag) {
+                    break;
+                }
+                if(j==s.length()-1){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/A25314.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class A25314 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int divNum=n/4;
+        for(int i=0;i<divNum;i++){
+            System.out.print("long ");
+        }
+        System.out.print("int");
+    }
+}
