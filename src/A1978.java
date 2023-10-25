ea8aa5da64adbaaa71bee6a65941a68ef0f822d4
ssung2sin <ssung2sin@naver.com>
2023-10-25 16:58:49 +0900

    1978'소수',1149'RGB거리'풀이

---
+++ src/A1978.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class A1978 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inputCount = sc.nextInt();
+        int count = 0;
+
+        for(int i = 0;i<inputCount;i++) {
+            int input = sc.nextInt();
+            if(input==1) {
+                continue;
+            }else if(input==2) {
+
+            }else{
+                for(int j=2;j<input;j++) {
+                    if(input%j==0) {
+                        count--;
+                        break;
+                    }
+                }
+            }
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
