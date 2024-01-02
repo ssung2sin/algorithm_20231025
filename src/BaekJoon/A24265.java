1c3756e2ea194cd6bfb12217a46736ab475d23e5
ssung2sin <ssung2sin@naver.com>
2024-01-02 15:26:03 +0900

    [백준]24265/10분/정답

---
+++ src/BaekJoon/A24265.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class A24265 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+		long tot = (n * (n - 1)) / 2;
+		System.out.println(tot);
+		System.out.println(2);
+
+	}
+}
