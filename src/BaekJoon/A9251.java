9b7da1b1c1d1d8a0b4753600335f2083f4581b1a
ssung2sin <ssung2sin@naver.com>
2024-01-21 00:32:08 +0900

    [백준]9251/2시간/정답(유튜브참고)

--- src/BaekJoon/A9251.java
+++ src/BaekJoon/A9251.java
@@ -7,14 +7,29 @@ import java.io.InputStreamReader;
 public class A9251 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String text1 = br.readLine();
-		String text2 = br.readLine();
-		int[][] lcs = new int[text1.length()][text2.length()];
-
+		StringBuilder text1 = new StringBuilder();
+		text1.append(0);
+		text1.append(br.readLine());
+		StringBuilder text2 = new StringBuilder();
+		text2.append(0);
+		text2.append(br.readLine());
+		int[][] lcs = new int[text1.length() + 1][text2.length() + 1];
+		boolean flag = false;
+		for (int i = 0; i < text2.length(); i++) {
+			lcs[0][i] = 0;
+		}
 		for (int i = 0; i < text1.length(); i++) {
-			for (int j = 0; j < text2.length(); j++) {
-				
+			lcs[i][0] = 0;
+		}
+		for (int i = 1; i < text1.length(); i++) {
+			for (int j = 1; j < text2.length(); j++) {
+				if (text1.charAt(i) == text2.charAt(j)) {
+					lcs[i][j] = lcs[i - 1][j - 1] + 1;
+				} else {
+					lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+				}
 			}
 		}
+		System.out.println(lcs[text1.length() - 1][text2.length() - 1]);
 	}
 }
