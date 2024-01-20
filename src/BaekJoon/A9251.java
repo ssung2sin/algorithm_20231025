acdbb7208a6441579f0dd051465ea53ec4b0d63d
ssung2sin <ssung2sin@naver.com>
2024-01-20 22:58:09 +0900

    [백준]9498/2분/정답(연습)

---
+++ src/BaekJoon/A9251.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A9251 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String text1 = br.readLine();
+		String text2 = br.readLine();
+		int[][] lcs = new int[text1.length()][text2.length()];
+
+		for (int i = 0; i < text1.length(); i++) {
+			for (int j = 0; j < text2.length(); j++) {
+				
+			}
+		}
+	}
+}
