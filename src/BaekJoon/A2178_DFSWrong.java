8b8997825c071feeb1114c5f48984b2dac2ba34b
ssung2sin <ssung2sin@naver.com>
2024-01-27 03:28:16 +0900

    [백준]2178/2시간30분/오답(시간초과)

--- src/BaekJoon/A2178_DFSWrong.java
+++ src/BaekJoon/A2178_DFSWrong.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class A2178 {
+public class A2178_DFSWrong {
 	private static int[][] count;
 	private static boolean[][] visit;
 
